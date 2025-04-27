ad53d2bcb658014c7cada2e4584e152a41adc818
baifeng <baifengbuzhidao@163.com>
2025-04-27 23:23:08 +0800

    初始化项目

---
+++ havenest-blog/src/main/java/com/only4play/havenest/blog/HavenestBlogApplication.java
@@ -0,0 +1,18 @@
+package com.only4play.havenest.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ *@title HavenestBlogApplication
+ *@description This is the main class of the Havenest Blog application
+ *@author macbookair
+ *@version 1.0.0
+ *@create 2025/4/27 22:57
+ **/
+@SpringBootApplication
+public class HavenestBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HavenestBlogApplication.class, args);
+    }
+}
